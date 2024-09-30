bc59f1ea2869929acec16144f89c941971c81e3b
RA341 <razerishi1505@gmail.com>
2024-09-29 20:31:39 -0400

    feat: added initial cli implementation

--- cmd/api/docker_impl.go
+++ cmd/api/docker_impl.go
@@ -10,27 +10,38 @@ type DockerAPI struct {
 
 // DockerContainerIdDelete Delete /docker/:containerId
 func (dk DockerAPI) DockerContainerIdDelete(c *gin.Context) {
-
+	log.Debug().Msgf("Recived Delete container request with id: %s", c.Param("containerId"))
+	c.Status(200)
 }
 
 // DockerContainerIdGet Get /docker/:containerId
 func (dk DockerAPI) DockerContainerIdGet(c *gin.Context) {
-
+	log.Debug().Msgf("Recived container info request with id: %s", c.Param("containerId"))
+	c.Status(200)
 }
 
 // DockerContainerIdStartGet Get /docker/:containerId/start
 func (dk DockerAPI) DockerContainerIdStartGet(c *gin.Context) {
-
+	log.Debug().Msgf("Recived start container request with id: %s", c.Param("containerId"))
+	c.Status(200)
 }
 
 // DockerContainerIdStopGet Get /docker/:containerId/stop
 func (dk DockerAPI) DockerContainerIdStopGet(c *gin.Context) {
-
+	log.Debug().Msgf("Recived stop container request with id: %s", c.Param("containerId"))
+	c.Status(200)
 }
 
 // DockerImagesCreatePost Post /docker/images/create
 func (dk DockerAPI) DockerImagesCreatePost(c *gin.Context) {
-	log.Debug().Msgf("Recived create image request")
+	form, err := c.MultipartForm()
+	if err != nil {
+		log.Error().Err(err).Msgf("Could not parse multipart form")
+		return
+	}
+	tagName := form.Value["tagName"]
+
+	log.Debug().Any("Tagname", tagName).Msgf("Recived create image request")
 	c.Status(200)
 }
 

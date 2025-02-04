0e004286af5ba13d407ef3b5c29da3c2584e29d2
RA341 <razerishi1505@gmail.com>
2025-02-04 17:29:06 -0500

    added more utils

--- src/tests/docker_test.go
+++ src/tests/docker_test.go
@@ -4,7 +4,7 @@ package tests
 //import (
 //	"context"
 //	"github.com/docker/docker/api/types/container"
-//	"github.com/makeopensource/leviathan/dockerclient"
+//	"github.com/makeopensource/leviathan/docker"
 //	"github.com/rs/zerolog"
 //	"github.com/rs/zerolog/log"
 //	"os"
@@ -16,25 +16,25 @@ package tests
 //func TestHelloName(t *testing.T) {
 //	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 //
-//	web_gen, err := dockerclient.NewLocalClient()
+//	web_gen, err := docker.NewLocalClient()
 //	if err != nil {
 //		log.Fatal().Msg("Failed to setup local docker web_gen")
 //	}
 //
-//	//web_gen, err := dockerclient.NewSSHClient("r334@192.168.50.123")
+//	//web_gen, err := docker.NewSSHClient("r334@192.168.50.123")
 //	//if err != nil {
 //	//	log.Fatal().Msg("Failed to setup docker web_gen")
 //	//}
 //
 //	log.Info().Msg("Connected to remote web_gen")
 //
-//	err = dockerclient.BuildImageFromDockerfile(web_gen, "../.example/ex-Dockerfile", "testimage:latest")
+//	err = docker.BuildImageFromDockerfile(web_gen, "../example/ex-Dockerfile", "testimage:latest")
 //	if err != nil {
 //		log.Error().Err(err).Msg("Failed to build image")
 //		return
 //	}
 //
-//	images, err := dockerclient.ListImages(web_gen)
+//	images, err := docker.ListImages(web_gen)
 //	if err != nil {
 //		log.Error().Msg("Failed to build image")
 //		return
@@ -44,7 +44,7 @@ package tests
 //		log.Info().Msgf("Container names: %v", image.RepoTags)
 //	}
 //
-//	newContainerId, err := dockerclient.CreateNewContainer(
+//	newContainerId, err := docker.CreateNewContainer(
 //		web_gen,
 //		"92912992939",
 //		"testimage:latest",
@@ -59,24 +59,24 @@ package tests
 //		return
 //	}
 //
-//	err = dockerclient.CopyToContainer(web_gen, newContainerId, "../.example/student/test.py")
+//	err = docker.CopyToContainer(web_gen, newContainerId, "../example/student/test.py")
 //	if err != nil {
 //		log.Error().Err(err).Msg("Failed to copy to container")
 //	}
 //
-//	err = dockerclient.StartContainer(web_gen, newContainerId)
+//	err = docker.StartContainer(web_gen, newContainerId)
 //	if err != nil {
 //		log.Error().Err(err).Msg("Failed to start container")
 //		return
 //	}
 //
-//	err, _ = dockerclient.TailContainerLogs(context.Background(), web_gen, newContainerId)
+//	err, _ = docker.TailContainerLogs(context.Background(), web_gen, newContainerId)
 //	if err != nil {
 //		log.Error().Err(err).Msg("Failed to tail logs")
 //		return
 //	}
 //
-//	data, err := dockerclient.ListContainers(web_gen)
+//	data, err := docker.ListContainers(web_gen)
 //	if err != nil {
 //		log.Error().Msg("Failed to build image")
 //		return
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,35 +1,20 @@
 package utils
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/rs/zerolog/log"
+	"io"
+	"os"
 	"strings"
 )
 
 const DefaultFilePerm = 0o775
-
-//func getFields(jsonMessage string) log.Fields {
-//	fields := log.Fields{}
-//	json.Unmarshal([]byte(jsonMessage), &fields)
-//	return fields
-//}
-//
-//func MultiLineResponseTrace(data string, message string) {
-//	scanner := bufio.NewScanner(strings.NewReader(data))
-//	for scanner.Scan() {
-//		log.WithFields(getFields(scanner.Text())).Trace(message)
-//	}
-//}
-//
-//func UserHomeDir() string {
-//	if runtime.GOOS == "windows" {
-//		home := os.Getenv("USERPROFILE")
-//		if home == "" {
-//			home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-//		}
-//		return home
-//	}
-//	return os.Getenv("HOME")
-//}
 
 func EncodeID(id1 string, id2 string) string {
 	return id1 + "#" + id2
@@ -41,4 +26,99 @@ func DecodeID(combinedId string) (string, string, error) {
 		return "", "", errors.New("could not decode ID")
 	}
 	return strs[0], strs[1], nil
+}
+
+// ArchiveJobData creates a tar.gz archive from the provided file map.
+func ArchiveJobData(files map[string][]byte) (io.ReadCloser, error) {
+	var buf bytes.Buffer
+	gz := gzip.NewWriter(&buf)
+	tw := tar.NewWriter(gz)
+
+	var filePerm int64 = 0600
+
+	// Add each file to the archive
+	for name, content := range files {
+		header := &tar.Header{
+			Name: name,
+			Mode: filePerm,
+			Size: int64(len(content)),
+		}
+		if err := tw.WriteHeader(header); err != nil {
+			return nil, err
+		}
+		if _, err := tw.Write(content); err != nil {
+			return nil, err
+		}
+	}
+	if err := tw.Close(); err != nil {
+		return nil, err
+	}
+	if err := gz.Close(); err != nil {
+		return nil, err
+	}
+
+	// we create a new reader so that
+	// we can close and cleanup the above tar/gzip writers
+	return io.NopCloser(bytes.NewReader(buf.Bytes())), nil
+}
+
+func ArchiveFiles(filePath string) (io.ReadCloser, error) {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to stat path %s", filePath)
+		return nil, err
+	}
+
+	archiveData, err := archive.Tar(filePath, archive.Gzip)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to archive %s", filePath)
+		return nil, err
+	}
+
+	return archiveData, nil
+}
+
+func GetLastLine(file *os.File) (string, error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return "", err
+	}
+
+	var lastLine string
+	buf := make([]byte, 1)
+	offset := stat.Size() - 1
+
+	for {
+		if offset < 0 {
+			break
+		}
+
+		_, err := file.Seek(offset, 0)
+		if err != nil {
+			return "", err
+		}
+
+		_, err = file.Read(buf)
+		if err != nil {
+			return "", err
+		}
+
+		if buf[0] == '\n' && lastLine != "" {
+			break
+		}
+
+		lastLine = string(buf) + lastLine
+		offset--
+	}
+
+	if lastLine == "" {
+		return "", fmt.Errorf("last line is empty")
+	}
+
+	return lastLine, nil
+}
+
+func IsValidJSON(s string) bool {
+	var js json.RawMessage
+	return json.Unmarshal([]byte(s), &js) == nil
 }

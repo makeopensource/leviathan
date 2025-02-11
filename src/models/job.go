cd40402284f5f64d08226e82372e7c95b45fde38
RA341 <razerishi1505@gmail.com>
2025-02-11 08:03:38 -0500

    added logstream writer

--- src/models/job.go
+++ src/models/job.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"connectrpc.com/connect"
+	v1 "github.com/makeopensource/leviathan/generated/jobs/v1"
 	"gorm.io/gorm"
 )
 
@@ -25,4 +27,18 @@ type Job struct {
 	StudentSubmissionFile     []byte
 	LabData                   LabModel `gorm:"-"`
 	OutputFilePath            string
+}
+
+// LogStreamWriter implements io.Writer interface,
+// to send docker output to a grpc stream
+type LogStreamWriter struct {
+	Stream *connect.ServerStream[v1.JobLogsResponse]
+}
+
+func (w *LogStreamWriter) Write(p []byte) (n int, err error) {
+	err = w.Stream.Send(&v1.JobLogsResponse{Logs: string(p)})
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
 }

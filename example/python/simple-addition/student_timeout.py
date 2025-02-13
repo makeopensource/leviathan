878fa444118f74d8d73536d57942bea1dbe77c62
RA341 <razerishi1505@gmail.com>
2025-02-13 00:13:20 -0500

    added timeout test

---
+++ example/python/simple-addition/student_timeout.py
@@ -0,0 +1,15 @@
+import time
+
+
+def add(a, b):
+    time.sleep(30) # add delay for test
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b

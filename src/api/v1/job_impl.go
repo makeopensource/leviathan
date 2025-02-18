c34060b9d8ba496f49aa6ccd7e342166a33aea46
RA341 <razerishi1505@gmail.com>
2025-02-18 16:21:12 -0500

    new job impl

--- src/api/v1/job_impl.go
+++ src/api/v1/job_impl.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"fmt"
 	v1 "github.com/makeopensource/leviathan/generated/jobs/v1"
+	types "github.com/makeopensource/leviathan/generated/types/v1"
 	"github.com/makeopensource/leviathan/models"
 	"github.com/makeopensource/leviathan/service/jobs"
+	"time"
 )
 
 type JobServer struct {
@@ -17,21 +19,26 @@ func (job *JobServer) NewJob(ctx context.Context, req *connect.Request[v1.NewJob
 	makeF := req.Msg.GetMakeFile()
 	grader := req.Msg.GetGraderFile()
 	stu := req.Msg.GetStudentSubmission()
-	tag := req.Msg.GetImageTag()
+	tag := req.Msg.GetImageName()
+	dockerfile := req.Msg.GetDockerFile()
 
-	newJob := models.Job{
-		ImageTag:                  tag,
-		StudentSubmissionFileName: stu.Filename,
-		StudentSubmissionFile:     stu.Content,
-		LabData: models.LabModel{
-			GraderFilename: grader.Filename,
-			GraderFile:     grader.Content,
-			MakeFilename:   makeF.Filename,
-			MakeFile:       makeF.Content,
-		},
+	vars := []*types.FileUpload{makeF, grader, stu, dockerfile}
+	for _, v := range vars {
+		if v == nil {
+			return nil, fmt.Errorf("some fields are empty")
+		} else if v.Filename == "" {
+			return nil, fmt.Errorf("filename is empty")
+		} else if len(v.Content) == 0 {
+			return nil, fmt.Errorf("empty content for file %s", v.Filename)
+		}
 	}
 
-	jobId, err := job.Service.NewJob(&newJob)
+	newJob := models.Job{
+		LabData:    models.LabModel{ImageTag: tag},
+		JobTimeout: time.Second * time.Duration(req.Msg.JobTimeoutInSeconds),
+	}
+
+	jobId, err := job.Service.NewJob(&newJob, makeF, grader, stu, dockerfile)
 	if err != nil {
 		return nil, err
 	}

a4eba441f084ba64998cc87dd3e1349b2c40b52f
RA341 <razerishi1505@gmail.com>
2025-02-18 09:35:54 -0500

    added index.ts

---
+++ spec/leviathan_node/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./generated/docker_rpc/v1/docker_pb"
+export * from "./generated/jobs/v1/jobs_pb"
+export * from "./generated/labs/v1/labs_pb"
+export * from "./generated/stats/v1/stats_pb"
+export * from "./generated/types/v1/types_pb"
+export * from "@connectrpc/connect-node"
+export * from "@connectrpc/connect"

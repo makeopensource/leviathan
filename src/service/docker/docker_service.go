86ddd226b6f9504f31bf3f2b24910f5ded7da9e0
RA341 <razerishi1505@gmail.com>
2025-02-21 08:05:32 -0500

    removed docker service methods since they are unused

--- src/service/docker/docker_service.go
+++ src/service/docker/docker_service.go
@@ -1,12 +1,8 @@
 package docker
 
 import (
-	"connectrpc.com/connect"
 	"context"
 	"fmt"
-	cont "github.com/docker/docker/api/types/container"
-	"github.com/makeopensource/leviathan/common"
-	dktypes "github.com/makeopensource/leviathan/generated/docker_rpc/v1"
 	"github.com/rs/zerolog/log"
 )
 
@@ -18,68 +14,7 @@ func NewDockerService(clientList *RemoteClientManager) *DkService {
 	return &DkService{ClientManager: clientList}
 }
 
-func (service *DkService) StartContainerReq(combinedId string) error {
-	containerId, machineId, err := common.ParseCombinedID(combinedId)
-	if err != nil {
-		return err
-	}
-
-	machine, err := service.ClientManager.GetClientById(machineId)
-	if err != nil {
-		return err
-	}
-
-	err = machine.StartContainer(containerId)
-	if err != nil {
-		log.Error().Err(err).Msgf("Failed to stop container at machine: %s with id", containerId)
-		return fmt.Errorf("failed to start container")
-	}
-	return nil
-}
-
-func (service *DkService) StopContainerReq(combinedId string) error {
-	containerId, machineId, err := common.ParseCombinedID(combinedId)
-	if err != nil {
-		return err
-	}
-
-	machine, err := service.ClientManager.GetClientById(machineId)
-	if err != nil {
-		return err
-	}
-
-	err = machine.StopContainer(containerId)
-	if err != nil {
-		log.Error().Err(err).Msgf("Failed to stop container at machine: %s with id", containerId)
-		return fmt.Errorf("failed to stop container")
-	}
-	return nil
-}
-
-func (service *DkService) ListImagesReq() []*dktypes.DockerImage {
-	var result []*dktypes.DockerImage
-
-	for machineID, cli := range service.ClientManager.Clients {
-		images, err := cli.Client.ListImages()
-		if err != nil {
-			info, err := cli.Client.Client.Info(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msg("failed to get docker server info")
-				continue
-			}
-			log.Error().Err(err).Msgf("Error listing images for %s", info.Name)
-			continue
-		}
-
-		result = append(result, &dktypes.DockerImage{
-			Id:       machineID,
-			Metadata: images,
-		})
-	}
-	return result
-}
-
-func (service *DkService) NewImageReq(dockerfilePath string, imageTag string) error {
+func (service *DkService) BuildNewImageOnAllClients(dockerfilePath string, imageTag string) error {
 	for _, item := range service.ClientManager.Clients {
 		err := item.Client.BuildImageFromDockerfile(dockerfilePath, imageTag)
 		if err != nil {
@@ -94,62 +29,4 @@ func (service *DkService) NewImageReq(dockerfilePath string, imageTag string) er
 	}
 
 	return nil
-}
-
-func (service *DkService) ListContainerReq() []*dktypes.DockerContainer {
-	var result []*dktypes.DockerContainer
-	for machineID, cli := range service.ClientManager.Clients {
-		info, err := cli.Client.Client.Info(context.Background())
-		if err != nil {
-			log.Error().Err(err).Msg("failed to get docker server info")
-			continue
-		}
-		containers, err := cli.Client.ListContainers(info.ID)
-		if err != nil {
-			log.Error().Err(err).Msgf("Error listing containers for %s", info.Name)
-			continue
-		}
-
-		result = append(result, &dktypes.DockerContainer{
-			Id:       machineID,
-			Metadata: containers,
-		})
-
-	}
-	return result
-}
-
-func (service *DkService) StreamContainerLogs(combinedId string, responseStream *connect.ServerStream[dktypes.GetContainerLogResponse]) error {
-	return nil
-}
-
-func (service *DkService) CreateContainerReq(machineId string, jobId string, imageTag string) (string, error) {
-	if machineId == "" {
-		return "", fmt.Errorf("machineId is empty or missing")
-	}
-	if jobId == "" {
-		return "", fmt.Errorf("jobID is empty or missing")
-	}
-	if imageTag == "" {
-		return "", fmt.Errorf("imageTag is empty or missing")
-	}
-
-	machine, err := service.ClientManager.GetClientById(machineId)
-	if err != nil {
-		log.Warn().Err(err).Msgf("Failed to get machine info")
-		return "", fmt.Errorf("failed to get machine info")
-	}
-
-	resources := cont.Resources{
-		Memory:   512 * 1000000,
-		NanoCPUs: 2 * 1000000000,
-	}
-
-	containerID, err := machine.CreateNewContainer(jobId, imageTag, "", resources)
-	if err != nil {
-		log.Error().Err(err).Msgf("Failed to create container for job %s", jobId)
-		return "", err
-	}
-
-	return containerID, nil
 }

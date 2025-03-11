f1b20fe6cd434dbb830af9eb7da04b831cc8310c
RA341 <razerishi1505@gmail.com>
2025-03-10 22:25:32 -0400

    refactored InitConfig
    added env overrides if present
    added Log level control to config

--- src/common/config.go
+++ src/common/config.go
@@ -4,16 +4,21 @@ import (
 	"errors"
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/makeopensource/leviathan/models"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func InitConfig() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Warn().Err(err).Msg(".env not found. you can safely ignore this warning if you dont have a .env file")
+	_, ok := os.LookupEnv("LEVIATHAN_IS_DOCKER")
+	if !ok {
+		err := godotenv.Load() // load .env file for non docker env
+		if err != nil {
+			log.Warn().Err(err).Msg(".env not found. ignore this warning if you did not intend to load a .env file")
+		}
 	}
 
 	defer func() {
@@ -27,17 +32,31 @@ func InitConfig() {
 	configDir := getConfigDir(baseDir)
 
 	viper.SetConfigName("config")
-	viper.SetConfigType("json")
+	viper.SetConfigType("toml")
 	viper.AddConfigPath(configDir)
 
-	setupDefaultOptions(configDir)
+	// ignore any error to setup default vals
+	_ = viper.ReadInConfig()
+
+	setIfEnvPresentOrDefault(
+		loglevelKey,
+		"LEVIATHAN_LOG_LEVEL",
+		"debug",
+	)
+
 	loadPostgresOptions()
+	setupDefaultOptions(configDir)
 
-	submissionFolderPath := getStringEnvOrDefault("TMP_SUBMISSION_FOLDER", fmt.Sprintf("%s/%s", baseDir, "submissions"))
-	viper.SetDefault(submissionDirKey, submissionFolderPath)
-
-	outputFolderPath := getStringEnvOrDefault("LOG_OUTPUT_FOLDER", fmt.Sprintf("%s/%s", baseDir, "output"))
-	viper.SetDefault(outputDirKey, outputFolderPath)
+	submissionFolderPath := setIfEnvPresentOrDefault(
+		submissionDirKey,
+		"TMP_SUBMISSION_DIR",
+		fmt.Sprintf("%s/%s", baseDir, "submissions"),
+	)
+	outputFolderPath := setIfEnvPresentOrDefault(
+		outputDirKey,
+		"SUBMISSION_OUTPUT_DIR",
+		fmt.Sprintf("%s/%s", baseDir, "output"),
+	)
 
 	err = makeDirectories([]string{submissionFolderPath, outputFolderPath})
 	if err != nil {
@@ -46,7 +65,13 @@ func InitConfig() {
 
 	if err := viper.SafeWriteConfig(); err != nil {
 		var configFileAlreadyExistsError viper.ConfigFileAlreadyExistsError
-		if !errors.As(err, &configFileAlreadyExistsError) {
+		if errors.As(err, &configFileAlreadyExistsError) {
+			// merge any new changes
+			err := viper.WriteConfig()
+			if err != nil {
+				log.Fatal().Err(err).Msg("viper could not write to config file")
+			}
+		} else {
 			log.Fatal().Err(err).Msg("viper could not write to config file")
 		}
 	}
@@ -55,46 +80,23 @@ func InitConfig() {
 		log.Fatal().Err(err).Msg("could not read config file")
 	}
 
-	log.Info().Msgf("watching config file at %s", viper.ConfigFileUsed())
-	viper.WatchConfig()
-
-	// maybe create viper instance and return from this function
-	// future setup in case https://github.com/spf13/viper/issues/1855 is accepted
+	log.Info().Msgf("loaded config from %s", viper.ConfigFileUsed())
 }
 
 func loadPostgresOptions() {
-	enablePost := false
-	if getStringEnvOrDefault("POSTGRES_ENABLE", "false") == "true" {
-		enablePost = true
+	setIfEnvPresentOrDefault(postgresHostKey, "POSTGRES_HOST", "localhost")
+	setIfEnvPresentOrDefault(postgresPortKey, "POSTGRES_PORT", "5432")
+	setIfEnvPresentOrDefault(postgresUserKey, "POSTGRES_USER", "postgres")
+	setIfEnvPresentOrDefault(postgresPassKey, "POSTGRES_PASSWORD", "postgres")
+	setIfEnvPresentOrDefault(postgresDBKey, "POSTGRES_DB", "postgres")
+	setIfEnvPresentOrDefault(postgresSslKey, "POSTGRES_SSL", "disable")
+
+	val, isDefault := getBoolEnvOrDefault("POSTGRES_ENABLE", false)
+	if isDefault {
+		viper.SetDefault(enablePostgresKey, val)
+	} else {
+		viper.Set(enablePostgresKey, val)
 	}
-	viper.SetDefault(
-		enablePostgresKey,
-		enablePost,
-	)
-	viper.SetDefault(
-		postgresHostKey,
-		getStringEnvOrDefault("POSTGRES_HOST", "localhost"),
-	)
-	viper.SetDefault(
-		postgresPortKey,
-		getStringEnvOrDefault("POSTGRES_PORT", "5432"),
-	)
-	viper.SetDefault(
-		postgresUserKey,
-		getStringEnvOrDefault("POSTGRES_USER", "postgres"),
-	)
-	viper.SetDefault(
-		postgresPassKey,
-		getStringEnvOrDefault("POSTGRES_PASSWORD", ""),
-	)
-	viper.SetDefault(
-		postgresDBKey,
-		getStringEnvOrDefault("POSTGRES_DB", "postgres"),
-	)
-	viper.SetDefault(
-		postgresSslKey,
-		getStringEnvOrDefault("POSTGRES_SSL", "disable"),
-	)
 }
 
 func getConfigDir(baseDir string) string {
@@ -106,12 +108,42 @@ func getConfigDir(baseDir string) string {
 	return configDir
 }
 
-func getStringEnvOrDefault(key, defaultVal string) string {
+// uses viper.Set if env var was found,
+//
+// else uses' viper.SetDefault and uses defaultValue
+//
+// this allows us to overwrite any new configration changes passed via env vars,
+// but ignore if no env were passed
+func setIfEnvPresentOrDefault(configKey, envKeyName, defaultValue string) string {
+	val, isDefault := getStringEnvOrDefault(envKeyName, defaultValue)
+	if isDefault {
+		viper.SetDefault(configKey, val)
+	} else {
+		// always overwrite with key
+		viper.Set(configKey, val)
+	}
+
+	return val
+}
+
+func getStringEnvOrDefault(key, defaultVal string) (finalVal string, isDefault bool) {
 	value := os.Getenv(key)
 	if value == "" {
-		return defaultVal
+		return defaultVal, true
 	}
-	return value
+	return value, false
+}
+
+func getBoolEnvOrDefault(key string, defaultVal bool) (finalVal, isDefault bool) {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultVal, true
+	}
+	parseBool, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultVal, true
+	}
+	return parseBool, false
 }
 
 func setupDefaultOptions(configDir string) {
@@ -121,6 +153,15 @@ func setupDefaultOptions(configDir string) {
 	viper.SetDefault(serverPortKey, "9221")
 	viper.SetDefault(enableLocalDockerKey, true)
 	viper.SetDefault(concurrentJobsKey, 50)
+	viper.SetDefault(ClientSSHKey, map[string]models.MachineOptions{
+		"example": {
+			Enable: false,
+			Name:   "example",
+			Host:   "http://localhost:8080",
+			User:   "test",
+			Port:   22,
+		},
+	})
 }
 
 func getBaseDir() (string, error) {

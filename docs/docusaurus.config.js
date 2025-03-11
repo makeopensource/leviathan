714e63b1d9a644c5bcef100d5092264c153b0c64
RA341 <razerishi1505@gmail.com>
2025-03-11 13:59:52 -0400

    removed redundant files

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,103 +4,102 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'Leviathan',
-  tagline: 'Container orchestrator and job runner',
-  url: 'https://makeopensource.github.io',
-  baseUrl: '/leviathan/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'makeopensource',
-  projectName: 'leviathan',
+    title: 'Leviathan',
+    tagline: 'Container orchestrator and job runner',
+    url: 'https://makeopensource.github.io',
+    baseUrl: '/leviathan/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'makeopensource',
+    projectName: 'leviathan',
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/makeopensource/leviathan/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-              'https://github.com/makeopensource/leviathan/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/makeopensource/leviathan/edit/main/website/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    editUrl:
+                        'https://github.com/makeopensource/leviathan/edit/main/website/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        navbar: {
-          title: 'Leviathan',
-          logo: {
-            alt: 'My Site Logo',
-            src: 'img/logo.svg',
-          },
-          items: [
-            {
-              type: 'doc',
-              docId: 'intro',
-              position: 'left',
-              label: 'Tutorial',
-            },
-            {to: '/blog', label: 'Blog', position: 'left'},
-            {
-              href: 'https://github.com/makeopensource/leviathan',
-              label: 'GitHub',
-              position: 'right',
-            },
-          ],
-        },
-        footer: {
-          style: 'dark',
-          links: [
-            {
-              title: 'Docs',
-              items: [
-                {
-                  label: 'Tutorial',
-                  to: '/docs/intro',
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'Leviathan',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
                 },
-              ],
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Tutorial',
+                    },
+                    // remove this comment to re add blog
+                    // {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href: 'https://github.com/makeopensource/leviathan',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
             },
-            {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Discord',
-                  href: 'https://discord.gg/ChrT2DfcDT',
-                },
-              ],
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Discord',
+                                href: 'https://discord.gg/ChrT2DfcDT',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/makeopensource/leviathan',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Leviathan. Built with Docusaurus.`,
             },
-            {
-              title: 'More',
-              items: [
-                {
-                  label: 'Blog',
-                  to: '/blog',
-                },
-                {
-                  label: 'GitHub',
-                  href: 'https://github.com/makeopensource/leviathan',
-                },
-              ],
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
             },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} Leviathan. Built with Docusaurus.`,
-        },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
-        },
-      }),
+        }),
 });

3b9f015623ec6a55bdd99b4b02cb9d15eb5f7f6a
RA341 <razerishi1505@gmail.com>
2025-03-21 21:25:26 -0400

    added ssh password auth

--- src/models/machine.go
+++ src/models/machine.go
@@ -1,9 +1,11 @@
 package models
 
 type MachineOptions struct {
-	Enable bool   `mapstructure:"enable"`
-	Name   string `mapstructure:"name"`
-	Host   string `mapstructure:"host"`
-	Port   int    `mapstructure:"port"`
-	User   string `mapstructure:"user"`
+	Enable    bool   `mapstructure:"enable"`
+	Name      string `mapstructure:"name"`
+	Host      string `mapstructure:"host"`
+	Port      int    `mapstructure:"port"`
+	User      string `mapstructure:"user"`
+	Password  string `mapstructure:"password"`
+	Publickey string `mapstructure:"host_publickey"`
 }

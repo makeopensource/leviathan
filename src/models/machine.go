4b9bce0ccb9182f95d79ca61e3adb0d8e7963e45
RA341 <razerishi1505@gmail.com>
2025-03-22 16:23:15 -0400

    added methods

--- src/models/machine.go
+++ src/models/machine.go
@@ -1,11 +1,70 @@
 package models
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 type MachineOptions struct {
-	Enable    bool   `mapstructure:"enable"`
-	Name      string `mapstructure:"name"`
-	Host      string `mapstructure:"host"`
-	Port      int    `mapstructure:"port"`
-	User      string `mapstructure:"user"`
-	Password  string `mapstructure:"password"`
-	Publickey string `mapstructure:"host_publickey"`
+	name             string // use get/set so that this field is not written to the config file
+	Enable           bool   `mapstructure:"enable"`
+	Host             string `mapstructure:"host"`
+	Port             int    `mapstructure:"port"`
+	User             string `mapstructure:"user"`
+	Password         string `mapstructure:"password"`
+	RemotePublickey  string `json:"remote_public_key"`
+	UsePublicKeyAuth bool   `json:"use_public_key_auth"`
+}
+
+func (opts *MachineOptions) Log() string {
+	var result strings.Builder
+	v := reflect.ValueOf(*opts)
+	typeOfS := v.Type()
+
+	result.WriteString("options:\n")
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldName := typeOfS.Field(i).Name
+		tag := typeOfS.Field(i).Tag
+
+		// Skip field
+		if fieldName == "RemotePublickey" || fieldName == "Password" {
+			continue
+		}
+
+		// Check for mapstructure or json tags
+		mapstructureTag := tag.Get("mapstructure")
+		jsonTag := tag.Get("json")
+
+		var outputName string
+		if mapstructureTag != "" {
+			outputName = mapstructureTag
+		} else if jsonTag != "" {
+			outputName = jsonTag
+		} else {
+			outputName = fieldName
+		}
+
+		// Format the output based on the field type
+		switch field.Kind() {
+		case reflect.String:
+			result.WriteString(fmt.Sprintf("  %s: %q\n", outputName, field.String()))
+		case reflect.Bool:
+			result.WriteString(fmt.Sprintf("  %s: %t\n", outputName, field.Bool()))
+		case reflect.Int:
+			result.WriteString(fmt.Sprintf("  %s: %d\n", outputName, field.Int()))
+		default:
+			result.WriteString(fmt.Sprintf("  %s: %v\n", outputName, field.Interface())) // Handle other types if needed
+		}
+	}
+	return strings.TrimSpace(result.String())
+}
+
+func (opts *MachineOptions) Name() string {
+	return opts.name
+}
+
+func (opts *MachineOptions) SetName(name string) {
+	opts.name = name
 }

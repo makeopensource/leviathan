66efce26fa2b0217eacb8d38ba2bfcb5b649f3a2
RA341 <razerishi1505@gmail.com>
2025-03-28 16:09:15 -0400

    added entry commands

--- src/service/jobs/job_utils.go
+++ src/service/jobs/job_utils.go
@@ -11,6 +11,55 @@ import (
 	"path/filepath"
 )
 
+// AutodriverLimits limits to construct the autodriver cli args
+// more info - https://github.com/UB-CSE-IT/Autolab-Public-Documentation/blob/d6c2fb902d8cc0534794f20d6ef0bab871fe2602/The%20autograding%20process.md#running-the-job
+// todo verify assumptions
+type AutodriverLimits struct {
+	// limits the number of processes that can be started
+	ULimit int
+	// sets the maximum file size that can be created in bytes I assume
+	MaxFileSize int
+	// limits the size of the output in bytes I assume
+	MaxOutputSize int
+	// sets the job timeout in seconds I assume
+	Timeout int
+}
+
+const (
+	// DefaultLeviathanEntryCommand command for normal graders made for leviathan
+	DefaultLeviathanEntryCommand = "cd autolab;"
+	// DefaultTangoEntryCommand command for tango compatibility
+	// sets up autograde cli
+	DefaultTangoEntryCommand = "" +
+		"chown autolab:autolab autolab; chmod 755 autolab;" + // mode perm since CopyToContainer switches to root and messes up perms
+		"su autolab -c \"autodriver -u 100 -f 104857600 -t 20 -o 1024000 autolab > output/feedback 2>&1\"; " +
+		"cat output/feedback;" // print stdout so that leviathan can parse
+)
+
+var (
+	// DefaultAutoDriverLimits limits taken from https://github.com/UB-CSE-IT/Autolab-Public-Documentation/blob/d6c2fb902d8cc0534794f20d6ef0bab871fe2602/The%20autograding%20process.md#running-the-job
+	DefaultAutoDriverLimits = AutodriverLimits{
+		ULimit:        100,
+		MaxFileSize:   104857600,
+		Timeout:       20,
+		MaxOutputSize: 1024000,
+	}
+)
+
+// builds the autodriver command for tango compatibility
+// defaults to DefaultAutoDriverLimits, if limits is nil
+func createTangoEntryCommand(limits *AutodriverLimits) string {
+	if limits == nil {
+		limits = &DefaultAutoDriverLimits
+	}
+	return fmt.Sprintf(""+
+		"chown autolab:autolab autolab; chmod 755 autolab;"+ // mode perm since CopyToContainer switches to root and messes up perms
+		"su autolab -c \"autodriver -u %d -f %d -t %d -o %d autolab > output/feedback 2>&1\";"+
+		"cat output/feedback;", // print stdout so that leviathan can parse
+		limits.ULimit, limits.MaxFileSize, limits.Timeout, limits.MaxOutputSize,
+	)
+}
+
 // HardLinkFolder creates hard links of all files from source folder to target folder
 // and maintains the original UID/GID
 func HardLinkFolder(sourceDir, targetDir string) error {

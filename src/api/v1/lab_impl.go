1658a3c54bf29861b9ebc131ef8b2c9eca70fd78
RA341 <razerishi1505@gmail.com>
2025-03-30 13:55:00 -0400

    switched to fileids for file transfer

--- src/api/v1/lab_impl.go
+++ src/api/v1/lab_impl.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	v1 "github.com/makeopensource/leviathan/generated/labs/v1"
+	typesv1 "github.com/makeopensource/leviathan/generated/types/v1"
 	"github.com/makeopensource/leviathan/models"
 	"github.com/makeopensource/leviathan/service/labs"
 	"time"
@@ -14,27 +15,28 @@ type LabServer struct {
 	Srv *labs.LabService
 }
 
-func (l LabServer) NewLab(ctx context.Context, req *connect.Request[v1.LabRequest]) (*connect.Response[v1.NewLabResponse], error) {
-	if req.Msg.LabName == "" {
+func (l LabServer) NewLab(ctx context.Context, req *connect.Request[v1.NewLabRequest]) (*connect.Response[v1.NewLabResponse], error) {
+	if req.Msg.LabData.Labname == "" {
 		return nil, fmt.Errorf("lab name is required")
-	} else if req.Msg.EntryCommand == "" {
-		return nil, fmt.Errorf("entry command is required")
-	} else if req.Msg.TimeLimitInSeconds == 0 {
+	} else if req.Msg.LabData.JobTimeoutInSeconds == 0 {
 		return nil, fmt.Errorf("0 seconds as time limit is not allowed")
+	} else if req.Msg.TmpFolderId == "" {
+		return nil, fmt.Errorf("tmp folder id is required")
 	}
 
 	lab := &models.Lab{
-		Name:       req.Msg.LabName,
-		JobTimeout: time.Duration(req.Msg.TimeLimitInSeconds) * time.Second,
+		Name:       req.Msg.LabData.Labname,
+		JobTimeout: time.Duration(req.Msg.LabData.JobTimeoutInSeconds) * time.Second,
 		JobLimits: models.MachineLimits{
-			PidsLimit: int64(req.Msg.MachineLimits.PidLimit),
-			NanoCPU:   int64(req.Msg.MachineLimits.CPUCores),
-			Memory:    int64(req.Msg.MachineLimits.MemoryInMb),
+			PidsLimit: int64(req.Msg.LabData.Limits.PidLimit),
+			NanoCPU:   int64(req.Msg.LabData.Limits.CPUCores),
+			Memory:    int64(req.Msg.LabData.Limits.MemoryInMb),
 		},
-		JobEntryCmd: req.Msg.EntryCommand,
+		JobEntryCmd:       req.Msg.LabData.EntryCmd,
+		AutolabCompatible: req.Msg.LabData.AutolabCompatibilityMode,
 	}
 
-	labID, err := l.Srv.CreateLab(lab, req.Msg.DockerFile, req.Msg.JobFiles)
+	labID, err := l.Srv.CreateLab(lab, req.Msg.TmpFolderId)
 	if err != nil {
 		return nil, err
 	}
@@ -43,10 +45,12 @@ func (l LabServer) NewLab(ctx context.Context, req *connect.Request[v1.LabReques
 	return res, nil
 }
 
-func (l LabServer) EditLab(ctx context.Context, req *connect.Request[v1.LabRequest]) (*connect.Response[v1.EditLabResponse], error) {
-	return nil, fmt.Errorf("unimplmented methods")
+func (l LabServer) EditLab(ctx context.Context, c *connect.Request[typesv1.LabData]) (*connect.Response[v1.EditLabResponse], error) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func (l LabServer) DeleteLab(ctx context.Context, req *connect.Request[v1.DeleteLabRequest]) (*connect.Response[v1.DeleteLabResponse], error) {
+
 	return nil, fmt.Errorf("unimplmented methods")
 }

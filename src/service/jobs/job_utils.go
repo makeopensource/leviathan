ed225ad02c225de20b58fb915e3d762c0d9dcdfa
RA341 <razerishi1505@gmail.com>
2025-03-30 13:57:27 -0400

    moved hardlink func to common.utils

--- src/service/jobs/job_utils.go
+++ src/service/jobs/job_utils.go
@@ -3,178 +3,18 @@ package jobs
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/makeopensource/leviathan/common"
-	v1 "github.com/makeopensource/leviathan/generated/types/v1"
 	"github.com/rs/zerolog/log"
-	"io/fs"
 	"os"
-	"path/filepath"
 )
-
-// AutodriverLimits limits to construct the autodriver cli args
-// more info - https://github.com/UB-CSE-IT/Autolab-Public-Documentation/blob/d6c2fb902d8cc0534794f20d6ef0bab871fe2602/The%20autograding%20process.md#running-the-job
-// todo verify assumptions
-type AutodriverLimits struct {
-	// limits the number of processes that can be started
-	ULimit int
-	// sets the maximum file size that can be created in bytes I assume
-	MaxFileSize int
-	// limits the size of the output in bytes I assume
-	MaxOutputSize int
-	// sets the job timeout in seconds I assume
-	Timeout int
-}
-
-const (
-	// DefaultLeviathanEntryCommand command for normal graders made for leviathan
-	DefaultLeviathanEntryCommand = "cd autolab;"
-	// DefaultTangoEntryCommand command for tango compatibility
-	// sets up autograde cli
-	DefaultTangoEntryCommand = "" +
-		"chown autolab:autolab autolab; chmod 755 autolab;" + // mode perm since CopyToContainer switches to root and messes up perms
-		"su autolab -c \"autodriver -u 100 -f 104857600 -t 20 -o 1024000 autolab > output/feedback 2>&1\"; " +
-		"cat output/feedback;" // print stdout so that leviathan can parse
-)
-
-var (
-	// DefaultAutoDriverLimits limits taken from https://github.com/UB-CSE-IT/Autolab-Public-Documentation/blob/d6c2fb902d8cc0534794f20d6ef0bab871fe2602/The%20autograding%20process.md#running-the-job
-	DefaultAutoDriverLimits = AutodriverLimits{
-		ULimit:        100,
-		MaxFileSize:   104857600,
-		Timeout:       20,
-		MaxOutputSize: 1024000,
-	}
-)
-
-// builds the autodriver command for tango compatibility
-// defaults to DefaultAutoDriverLimits, if limits is nil
-func createTangoEntryCommand(limits *AutodriverLimits) string {
-	if limits == nil {
-		limits = &DefaultAutoDriverLimits
-	}
-	return fmt.Sprintf(""+
-		"chown autolab:autolab autolab; chmod 755 autolab;"+ // mode perm since CopyToContainer switches to root and messes up perms
-		"su autolab -c \"autodriver -u %d -f %d -t %d -o %d autolab > output/feedback 2>&1\";"+
-		"cat output/feedback;", // print stdout so that leviathan can parse
-		limits.ULimit, limits.MaxFileSize, limits.Timeout, limits.MaxOutputSize,
-	)
-}
-
-// HardLinkFolder creates hard links of all files from source folder to target folder
-// and maintains the original UID/GID
-func HardLinkFolder(sourceDir, targetDir string) error {
-	// Check if source directory exists
-	sourceStat, err := os.Stat(sourceDir)
-	if err != nil {
-		return fmt.Errorf("source directory error: %w", err)
-	}
-
-	// Create target directory if it doesn't exist
-	err = os.MkdirAll(targetDir, common.DefaultFilePerm)
-	if err != nil {
-		return fmt.Errorf("failed to create target directory: %w", err)
-	}
-
-	// Handle based on whether source is file or directory
-	if !sourceStat.IsDir() {
-		// For single file, create the parent directory if needed
-		sourceFile := filepath.Base(sourceDir)
-		log.Debug().Msgf("Target path is: %s", sourceFile)
-
-		targetDir = fmt.Sprintf("%s/%s", targetDir, sourceFile)
-		log.Info().Err(err).Msgf("Source is a file, final dest path will be %s", targetDir)
-
-		// Create hard link for the file
-		if err := createHardLink(sourceDir, targetDir, sourceStat.Mode()); err != nil {
-			return fmt.Errorf("failed to create hard link from %s to %s: %w", sourceDir, targetDir, err)
-		}
-
-		return nil
-	}
-
-	// Walk through the source directory
-	return filepath.WalkDir(sourceDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Get file info including system info (UID/GID)
-		info, err := d.Info()
-		if err != nil {
-			return fmt.Errorf("failed to get file info: %w", err)
-		}
-
-		// Get relative path
-		relPath, err := filepath.Rel(sourceDir, path)
-		if err != nil {
-			return fmt.Errorf("failed to get relative path: %w", err)
-		}
-
-		// Get target path
-		targetPath := filepath.Join(targetDir, relPath)
-
-		// If it's a directory, create it in target with proper ownership
-		if d.IsDir() {
-			if err := os.MkdirAll(targetPath, common.DefaultFilePerm); err != nil {
-				return fmt.Errorf("failed to create directory %s: %w", targetPath, err)
-			}
-
-			return nil
-		}
-
-		// Create hard link for files
-		err = createHardLink(path, targetPath, info.Mode())
-		if err != nil {
-			return fmt.Errorf("failed to create hard link from %s to %s: %w", path, targetPath, err)
-		}
-
-		return nil
-	})
-}
-
-// createHardLink creates a hard link with proper ownership and permissions
-func createHardLink(oldPath, newPath string, mode fs.FileMode) error {
-	// Ensure the target directory exists
-	targetDir := filepath.Dir(newPath)
-	if err := os.MkdirAll(targetDir, 0755); err != nil {
-		return err
-	}
-
-	// Remove existing file if it exists
-	if err := os.Remove(newPath); err != nil && !os.IsNotExist(err) {
-		return err
-	}
-
-	// Create the hard link
-	if err := os.Link(oldPath, newPath); err != nil {
-		return err
-	}
-
-	if err := os.Chmod(newPath, mode); err != nil {
-		return fmt.Errorf("failed to set permissions: %w", err)
-	}
-
-	return nil
-}
 
 // CreateTmpJobDir sets up a throwaway dir to store submission files
 // you might be wondering why the '/autolab' subdir, TarDir untars it under its parent dir,
 // so in container this will unpack with 'autolab' as the parent folder
 // why not modify TarDir I tried and, this was easier than modifying whatever is going in that function
-func CreateTmpJobDir(uuid, baseFolder string, files ...*v1.FileUpload) (string, error) {
+func CreateTmpJobDir(uuid, baseFolder string) (string, error) {
 	tmpFolder, err := createJobFolder(uuid, baseFolder)
 	if err != nil {
 		return "", err
-	}
-
-	for _, file := range files {
-		if err := os.WriteFile(
-			fmt.Sprintf("%s/%s", tmpFolder, file.Filename),
-			file.Content,
-			common.DefaultFilePerm,
-		); err != nil {
-			return "", err
-		}
 	}
 
 	return tmpFolder, nil

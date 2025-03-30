afa01449b8ef55a04f4d9a647369b13782140254
RA341 <razerishi1505@gmail.com>
2025-03-29 21:24:42 -0400

    added file upload funcs

--- spec/leviathan_node/src/index.ts
+++ spec/leviathan_node/src/index.ts
@@ -5,3 +5,48 @@ export * from "./generated/stats/v1/stats_pb"
 export * from "./generated/types/v1/types_pb"
 export * from "@connectrpc/connect-node"
 export * from "@connectrpc/connect"
+
+export type FileData = {
+    fieldName: string,
+    filename: string,
+    filedata: Blob
+}
+
+export async function UploadLabFiles(basePath: String, files: Array<FileData>) {
+    const url = `${basePath}/v1/files/upload/submission`
+    return UploadMultipartForm(url, files)
+}
+
+export async function UploadSubmissionFiles(basePath: String, files: Array<FileData>) {
+    const url = `${basePath}/v1/files/upload/lab`
+    return UploadMultipartForm(url, files)
+}
+
+async function UploadMultipartForm(url: string, files: Array<FileData>,) {
+    const formData = new FormData();
+
+    for (const file of files) {
+        formData.append(
+            file.fieldName,
+            file.filedata,
+            file.filename,
+        );
+    }
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error(`Upload failed with status: ${response.status}\n${response.statusText}\n${response.body}`);
+    }
+
+    const data = await response.json();
+    if (data && data.folderId) {
+        return data.folderId;
+    } else {
+        throw new Error('Response did not contain an folderID');
+    }
+}
+

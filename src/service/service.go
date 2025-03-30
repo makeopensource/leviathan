cd47933c2c09ba8d62da43d74f4fb8368c952a4c
RA341 <razerishi1505@gmail.com>
2025-03-30 13:56:11 -0400

    added file handler

--- src/service/service.go
+++ src/service/service.go
@@ -3,14 +3,18 @@ package service
 import (
 	"github.com/makeopensource/leviathan/common"
 	"github.com/makeopensource/leviathan/service/docker"
+	"github.com/makeopensource/leviathan/service/file_manager"
 	"github.com/makeopensource/leviathan/service/jobs"
 	"github.com/makeopensource/leviathan/service/labs"
 )
 
 func InitServices() (*docker.DkService, *jobs.JobService, *labs.LabService) {
 	db, bc := common.InitDB()
+
 	dkService := docker.NewDockerServiceWithClients()
-	labService := labs.NewLabService(db, dkService)
-	jobService := jobs.NewJobService(db, bc, dkService, labService)
+	fileManService := file_manager.NewFileManagerService()
+	labService := labs.NewLabService(db, dkService, fileManService)
+	jobService := jobs.NewJobService(db, bc, dkService, labService, fileManService)
+
 	return dkService, jobService, labService
 }

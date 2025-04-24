b26ebf4e3a66d16f5b2fe4eec7ce6cfdd6b6ecd8
RA341 <razerishi1505@gmail.com>
2025-04-23 23:34:27 -0400

    Revert "removed context"
    This reverts commit ce506c5d49d9b3206527104c36896cf3a7d31dbd.

--- src/models/job.go
+++ src/models/job.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"fmt"
 	v1 "github.com/makeopensource/leviathan/generated/jobs/v1"
 	"github.com/rs/zerolog/log"
@@ -60,6 +61,7 @@ type Job struct {
 	OutputLogFilePath string
 	// TmpJobFolderPath holds the path to the tmp dir all files related to the job except the final output
 	TmpJobFolderPath string
+	JobCtx           context.Context `gorm:"-"`
 }
 
 func (j *Job) ToProto() *v1.JobStatus {
@@ -86,6 +88,9 @@ func (j *Job) ValidateForQueue() error {
 	}
 	if j.LabData.JobTimeout == 0 {
 		return fmt.Errorf("job timeout is 0")
+	}
+	if j.JobCtx == nil {
+		return fmt.Errorf("job context is nil")
 	}
 	if j.OutputLogFilePath == "" {
 		return fmt.Errorf("output log file is empty")
